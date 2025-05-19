058337957df417241084862ba50b25d3b0865bad
JOAO PEDRO BORGES SOUZA SANTANA <202402717707@alunos.ibmec.edu.br>
2025-05-19 11:33:16 -0300

    aula left join, right join e union

--- sql/armazen1.sql
+++ sql/armazen1.sql
@@ -127,4 +127,84 @@ JOIN cliente AS cl ON co.idcliente = cl.idcliente
 JOIN produto AS pr ON co.idproduto = pr.idproduto
 JOIN loja AS lo ON co.idloja = lo.idloja;
 
+-- EX 21
+SELECT pr.nomeproduto, mr.nomemarca
+FROM produto AS pr, marca AS mr
+WHERE pr.idmarca = mr.idmarca;
+
+-- EX 22
+SELECT cl.nomecliente, pr.nomeproduto
+FROM compras AS cs, cliente AS cl, produto AS pr
+WHERE cl.idcliente = cs.idcliente
+AND cs.idproduto = pr.idproduto;
+
+SELECT COUNT(*) FROM compras;
+
+-- EX 23
+SELECT cl.nomecliente, cl.estado, lj.nomeloja, lj.uf
+FROM compras AS cs, cliente AS cl, loja AS lj
+WHERE cs.idcliente = cl.idcliente
+AND cs.idloja = lj.idloja
+AND qtd > '40';
+
+-- EX 24 
+SELECT pr.nomeproduto, pr.preco, mr.nomemarca
+FROM produto AS pr
+JOIN marca AS mr ON pr.idmarca = mr.idmarca;
+
+-- EX 25
+SELECT mr.nomemarca, cs.dtcompra, cs.qtd
+FROM produto AS pr
+JOIN marca AS mr ON pr.idmarca = mr.idmarca
+JOIN compras AS cs ON pr.idproduto = cs.idproduto;
+
+-- EX 26
+SELECT cl.nomecliente, pr.nomeproduto, cs.qtd
+FROM compras AS cs
+JOIN produto AS pr ON pr.idproduto = cs.idproduto
+JOIN cliente AS cl ON cl.idcliente = cs.idcliente;
+
+INSERT INTO cliente
+(idcliente,nomecliente,genero,estadocivil,dtnascimento,cidade,estado)
+VALUES
+(501, 'Alba', 'F', 'V', '1982/10/25', 'Duque de Caxias','Rio de Janeiro'),
+(502, 'Solimar', 'F', 'V', '1972/11/25', 'Niterói','Rio de Janeiro'),
+(503, 'Pablo', 'M', 'C', '1999/03/01', 'Nova Iguaçu','Rio de Janeiro'),
+(504, 'Solange', 'F', 'S', '1979/06/05', 'Salvador', 'Bahia'),
+(505, 'Mário', 'M', 'S', '1985/07/22', 'Bragança Paulista', 'São Paulo');
+
+-- EX 27 
+SELECT cl.nomecliente, cs.idcompra
+FROM cliente AS cl
+LEFT JOIN compras AS cs ON cl.idcliente = cs.idcliente;
+
+INSERT INTO marca (idmarca,nomemarca)
+VALUES
+(6,'Delícia de Prato'),
+(7,'Pajé');
+
+-- EX 28
+SELECT mr.nomemarca, pr.nomeproduto
+FROM produto AS pr 
+RIGHT JOIN marca AS mr ON mr.idmarca = pr.idmarca;
+
+-- EX 29
+SELECT cl.nomecliente, mr.nomemarca FROM cliente AS cl
+LEFT JOIN compras AS cs ON cl.idcliente = cs.idcliente
+LEFT JOIN produto AS pr ON cs.idproduto = pr.idproduto
+LEFT JOIN marca AS mr ON pr.idmarca = mr.idmarca
+
+UNION
+
+SELECT cl.nomecliente, mr.nomemarca
+FROM cliente AS cl
+RIGHT JOIN compras AS cs ON cl.idcliente = cs.idcliente
+RIGHT JOIN produto AS pr ON cs.idproduto = pr.idproduto
+RIGHT JOIN marca AS mr ON pr.idmarca = mr.idmarca;
+
+
+
+
+
+
 
