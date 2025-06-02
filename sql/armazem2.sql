798ad862f2b16db259ed3b6de2a7ef2515783f78
JOAO PEDRO BORGES SOUZA SANTANA <202402717707@alunos.ibmec.edu.br>
2025-06-02 11:13:37 -0300

    Visão e outras coisas

--- sql/armazem2.sql
+++ sql/armazem2.sql
@@ -133,3 +133,77 @@ SELECT nomecliente, COUNT(co.qtd) AS quantidade
 		JOIN compras AS co ON co.idcliente = cl.idcliente
 GROUP BY nomecliente
 	HAVING quantidade < 5;
+
+-- EX 24
+SELECT p.nomeproduto, p.preco
+	FROM produto AS p
+WHERE preco IN (
+	SELECT DISTINCT MAX(p.preco) 
+		FROM produto as p);
+    
+-- EX 25
+SELECT nomecliente
+	FROM cliente
+WHERE idcliente NOT IN(
+SELECT DISTINCT idcliente FROM compras
+)
+ORDER BY nomecliente; 
+
+-- EX 26
+SELECT nomeproduto
+	FROM produto
+WHERE idproduto NOT IN(
+SELECT DISTINCT idproduto 
+	FROM compras
+WHERE YEAR(dtcompra) = 2025
+);
+
+insert into compras (idcompra, idcliente, idproduto, dtcompra, idloja, qtd) values (1001, 177, 1, '2025-06-02', 1, 15);
+
+-- EX 27
+CREATE VIEW VW_produto AS 
+SELECT p.nomeproduto AS produtos, p.preco AS preco, m.nomemarca AS marca
+	FROM produto AS p
+		JOIN marca AS m ON p.idmarca = m.idmarca
+ORDER BY p.nomeproduto;
+
+SELECT * FROM VW_produto;
+	
+-- EX 28
+CREATE VIEW VW_descricao AS 
+SELECT co.idcompra AS idcompra, cl.nomecliente AS cliente, pr.nomeproduto AS produto, mr.nomemarca AS marca, lo.nomeloja AS loja, co.dtcompra AS datanascimento, co.qtd AS quantidade, pr.preco AS preco, SUM(qtd*preco) AS valor_total
+	FROM compras AS co
+		JOIN cliente AS cl ON cl.idcliente = co.idcliente
+        JOIN produto AS pr ON pr.idproduto = co.idproduto
+        JOIN marca AS mr ON mr.idmarca = pr.idmarca
+        JOIN loja AS lo ON lo.idloja = co.idloja
+GROUP BY co.idcompra
+ORDER BY co.idcompra;
+
+SELECT * FROM VW_descricao;
+
+-- EX 29
+SELECT count(qtd) FROM compras; 
+
+-- EX 30
+SELECT SUM(valor_total) FROM VW_descricao;
+
+-- EX 31
+SELECT MAX(valor_total) AS maiorcompra, MIN(valor_total) AS menorcompra FROM VW_descricao;
+
+SELECT cliente, valor_total
+	FROM VW_descricao 
+WHERE valor_total = (
+SELECT MAX(valor_total) AS maiorcompra
+FROM VW_descricao);
+
+-- EX 32
+SELECT ROUND(AVG(valor_total)) AS valor_medio FROM VW_descricao;
+
+-- EX 33
+SELECT COUNT(valor_total) AS comprasgrandes
+	FROM VW_descricao
+WHERE valor_total > (
+	SELECT ROUND(AVG(valor_total))
+		FROM VW_descricao
+);
